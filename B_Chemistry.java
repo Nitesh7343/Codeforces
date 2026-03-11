45e51d78fa2bdc4d9c277272ec456ae05158c165
Nitesh <niteshsingh7343@gmail.com>
2026-03-11 11:44:26 +0530

    Cemistry CF

--- B_Chemistry.java
+++ B_Chemistry.java
@@ -4,7 +4,21 @@ public class B_Chemistry {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-- > 0) {
-            
+            int n= sc.nextInt();
+            int k= sc.nextInt();
+
+            String str = sc.next();
+            HashMap<Character,Integer> map = new HashMap<>();
+            for(char c : str.toCharArray()) {
+                map.put(c,map.getOrDefault(c,0)+1);
+            }
+            int ev = 0;
+            int od = 0;
+            for(int num : map.values()) {
+                int temp = num%2 == 0 ? ev++ : od++;
+            }
+
+            System.out.println(od <= k+1 ? "YES" : "NO");
         }
     }
 }
