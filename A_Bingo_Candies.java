e17723612d0b01abcbda0e3bbe70b19b24d3d69d
Nitesh <niteshsingh7343@gmail.com>
2026-03-16 23:15:25 +0530

    Bingo vandies Problem CF

--- A_Bingo_Candies.java
+++ A_Bingo_Candies.java
@@ -5,26 +5,21 @@ public class A_Bingo_Candies {
 
         int t = sc.nextInt();
 
-        while(t-- > 0) {
+       while(t-- > 0) {
+        int n = sc.nextInt();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        boolean no = false;
 
-            int n = sc.nextInt();
-            HashMap<Integer,Integer> freq = new HashMap<>();
-
-            for(int i = 0; i < n*n; i++) {
-                int x = sc.nextInt();
-                freq.put(x, freq.getOrDefault(x,0) + 1);
-            }
-
-            boolean possible = true;
-
-            for(int f : freq.values()) {
-                if(f > n*(n-1)) {
-                    possible = false;
-                    break;
+        for(int i = 0; i < n;i++) {
+            for(int j = 0; j < n;j++) {
+                int num = sc.nextInt();
+                map.put(num,map.getOrDefault(num,0)+1);
+                if(map.get(num) > n*(n-1)) {
+                    no = true;
                 }
             }
-
-            System.out.println(possible ? "YES" : "NO");
         }
+        System.out.println(no ? "NO" : "YES");
+       }
     }
 }
