c45b66048d476bc259e143cde5e9c8f92191cad4
Nitesh <niteshsingh7343@gmail.com>
2026-03-17 23:33:53 +0530

    Solving Find the different ones

---
+++ D_Find_the_Different_Ones.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class D_Find_the_Different_Ones {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n;i++) {
+                arr[i] = sc.nextInt();
+            }
+            int q = sc.nextInt();
+            while(q-- > 0) {
+                int l = sc.nextInt();
+                int r = sc.nextInt();
+                
+            }
+        }
+        
+    }
+}
