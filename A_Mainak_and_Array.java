3572f5a34a75914067722f379a8b806a16093094
Nitesh <niteshsingh7343@gmail.com>
2026-03-17 23:33:30 +0530

    Remarks on Mainak and array

--- A_Mainak_and_Array.java
+++ A_Mainak_and_Array.java
@@ -14,9 +14,10 @@ public class A_Mainak_and_Array {
                 continue;
             }
             if(n == 2) {
-                System.out.println(Math.abs(arr[1]-arr[0]));
+                System.out.println(Math.abs(arr[1]-arr[0])); //use abs for n == 2
                 continue;
             }
+            // don't use abs anywhere
             int max = arr[n-1]-arr[0];
 
             int minT = Integer.MAX_VALUE;
@@ -25,7 +26,7 @@ public class A_Mainak_and_Array {
             for(int i = 0; i < n;i++) {
                 if(i != 0) maxT = Math.max(maxT,arr[i]);
                 if(i != n-1) minT = Math.min(minT,arr[i]);
-                if(i != 0) maxDiff = Math.max(maxDiff , arr[i-1]-arr[i]);
+                if(i != 0) maxDiff = Math.max(maxDiff , arr[i-1]-arr[i]); // take in reverse direction (arr[i-1]-arr[i]) not (arr[i]-arr[i-1])
             }
 
             max = Math.max(max, Math.max(maxDiff, Math.max(maxT-arr[0],arr[n-1]-minT)));
