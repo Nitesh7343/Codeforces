3ebe5c065f50d7f73c3ec05c659e1ff8bf4e025d
Nitesh <niteshsingh7343@gmail.com>
2026-03-17 23:33:11 +0530

    Mainak and Array Submitted

---
+++ A_Mainak_and_Array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class A_Mainak_and_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n;i++) {
+                arr[i] = sc.nextInt();
+            }
+            if(n == 1) {
+                System.out.println(0);
+                continue;
+            }
+            if(n == 2) {
+                System.out.println(Math.abs(arr[1]-arr[0]));
+                continue;
+            }
+            int max = arr[n-1]-arr[0];
+
+            int minT = Integer.MAX_VALUE;
+            int maxT = Integer.MIN_VALUE;
+            int maxDiff = Integer.MIN_VALUE;
+            for(int i = 0; i < n;i++) {
+                if(i != 0) maxT = Math.max(maxT,arr[i]);
+                if(i != n-1) minT = Math.min(minT,arr[i]);
+                if(i != 0) maxDiff = Math.max(maxDiff , arr[i-1]-arr[i]);
+            }
+
+            max = Math.max(max, Math.max(maxDiff, Math.max(maxT-arr[0],arr[n-1]-minT)));
+
+            System.out.println(max);
+        }
+    }
+}
