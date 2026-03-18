45c2ead686d8d5d3910fe910cd5bca798181a322
Nitesh <niteshsingh7343@gmail.com>
2026-03-18 23:13:26 +0530

    Odd_Grasshopper Solution pasted

--- B_Odd_Grasshopper.java
+++ B_Odd_Grasshopper.java
@@ -1,17 +1,23 @@
 import java.util.*;
 public class B_Odd_Grasshopper{
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
-        while(t-- > 0) {
+        while (t-- > 0) {
             long x = sc.nextLong();
             long n = sc.nextLong();
-            int i = 1;
-            while(i <= n) {
-                if(x%2 == 0) x = x-i;
-                else x = x+i;
 
-                i++;
+            long k = n % 4;
+
+            if (x % 2 == 0) {
+                if (k == 1) x -= n;
+                else if (k == 2) x += 1;
+                else if (k == 3) x += n + 1;
+            } else {
+                if (k == 1) x += n;
+                else if (k == 2) x -= 1;
+                else if (k == 3) x -= n + 1;
             }
 
             System.out.println(x);
