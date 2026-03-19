396179f8111bd12dd3a1e945a4a03b1e4d2a386c
Nitesh <niteshsingh7343@gmail.com>
2026-03-19 21:54:46 +0530

    Make it increasing CF solved

--- B_Make_It_Increasing.java
+++ B_Make_It_Increasing.java
@@ -9,7 +9,26 @@ public class B_Make_It_Increasing{
             for(int i = 0; i < n;i++) {
                 arr[i] = sc.nextLong();
             }
-            
+            if(n == 1) {
+                System.out.println(0);
+                continue;
+            }
+            int cnt = 0;
+            for(int i = n-1; i >= 1;i--) {
+                if(arr[i] == 1 && i >1) {
+                    cnt = -1;
+                    break;
+                }
+                while(arr[i] <= arr[i-1]) {
+                    if(arr[i-1] == 0) {
+                        cnt = -1;
+                        break;
+                    }
+                    arr[i-1] /= 2;
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
         }
     }
 }
