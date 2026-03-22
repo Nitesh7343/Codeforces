7c50868bcc6396071a362ed7e07c836be7467395
Nitesh <niteshsingh7343@gmail.com>
2026-03-22 23:53:04 +0530

    Array solved again

--- B_Array.java
+++ B_Array.java
@@ -3,23 +3,22 @@ public class B_Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
-        while (t-- > 0) {
+        while(t-- > 0) {
             int n = sc.nextInt();
-            long[] a = new long[n];
-            for (int i = 0; i < n; i++) a[i] = sc.nextLong();
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < n; i++) {
-                int less = 0, greater = 0;
-                for (int j = i + 1; j < n; j++) {
-                    if (a[j] < a[i]) less++;
-                    else if (a[j] > a[i]) greater++;
-                }
-                sb.append(Math.max(less, greater));
-                if (i < n - 1) sb.append(' ');
+            long[] arr = new long[n];
+            for(int i = 0; i < n;i++) {
+                arr[i] = sc.nextInt();
             }
-            System.out.println(sb);
+            for(int i = 0; i < n;i++) {
+                int min = 0;
+                int max = 0;
+                for(int j = i+1; j < n;j++) {
+                    if(arr[i] < arr[j]) max++;
+                    else if(arr[i] > arr[j]) min++;
+                }
+                System.out.print(min > max ? min+" " : max+" ");
+            }
+            System.out.println();
         }
     }
 }
