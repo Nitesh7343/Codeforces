47236922a10fe07dfceadd3c041d369d38c6034e
Nitesh <niteshsingh7343@gmail.com>
2026-03-24 22:56:28 +0530

    Optmized Registration system with HashMap.

--- C_Registration_system.java
+++ C_Registration_system.java
@@ -4,20 +4,18 @@ public class C_Registration_system {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String[] arr = new String[n];
-        for(int i = 0; i < n;i++) arr[i] = sc.next();
-        HashSet<String> set = new HashSet<>();
-        for(String s : arr) {
-            if(set.contains(s)) {
-                int j = 1;
-                while(set.contains(s+j)) {
-                    j++;
-                }
-                System.out.println(s+j);
-                set.add(s+j);
-                }
-            else {
+        HashMap<String,Integer> map = new HashMap<>();
+        while(n-- > 0) {
+            String s = sc.next();
+            if(map.containsKey(s)) {
+                int cnt = map.get(s);
+                String newS = s+cnt;
+                System.out.println(newS);
+                map.put(s,cnt+1);
+            }
+            else{
                 System.out.println("OK");
-                set.add(s);
+                map.put(s,1);
             }
         }
     }
