155c36a00e9197ac2d37f8bc5280ea70634f9845
Nitesh <niteshsingh7343@gmail.com>
2026-03-26 22:24:45 +0530

    Solved A game with integers

--- A_Game_with_Integers.java
+++ A_Game_with_Integers.java
@@ -1,6 +1,15 @@
 import java.util.*;
 public class A_Game_with_Integers {
     public static void main(String[] args) {
-        
-    }
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- > 0) { 
+			long n = sc.nextLong();
+			if (n % 3 == 0) {
+				System.out.println("Second");
+			} else {
+				System.out.println("First");
+			}
+		}
+	}
 }
