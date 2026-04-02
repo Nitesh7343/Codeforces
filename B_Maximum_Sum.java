f9b44c59bc820f8b54a293974fbf346239343ed1
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-02 23:22:03 +0530

    Solved Maximum Sum cf

--- B_Maximum_Sum.java
+++ B_Maximum_Sum.java
@@ -9,8 +9,22 @@ public class B_Maximum_Sum {
             long prefix[] = new long[n];
             long[] arr = new long[n];
             for(int i = 0; i < n;i++) {
-                
+                arr[i] = sc.nextLong();
             }
+            Arrays.sort(arr);
+            prefix[0] = arr[0];
+            for(int i =1; i < n;i++) {
+                prefix[i] = prefix[i-1]+arr[i];
+            }
+
+            long curr = 0;
+            long max = 0;
+            for(int i = 0; i <= k;i++) {
+                curr = 2*(k-i)-1 >= 0 ? prefix[n-i-1]-prefix[2*(k-i)-1] : prefix[n-i-1] ;
+                max = Math.max(curr,max);
+            }
+
+            System.out.println(max);
         }
     }
 }
