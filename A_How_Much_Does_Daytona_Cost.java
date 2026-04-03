4da50c65b0deb1726d5f65a3a1b3d059c19bb5fb
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-03 23:12:37 +0530

    solved A how much does dautona cost cf tle 800

--- A_How_Much_Does_Daytona_Cost.java
+++ A_How_Much_Does_Daytona_Cost.java
@@ -1,4 +1,18 @@
 import java.util.*;
 public class A_How_Much_Does_Daytona_Cost {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            boolean kk = false;
+            for(int i =0; i < n;i++) {
+                arr[i] = sc.nextInt();
+                if(arr[i] == k) kk = true;
+            }
+            System.out.println(kk ? "YES" : "NO");
+        }
+    }
 }
