cb2860021aab6b7d387d3592b35dcae45a79725e
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-06 21:57:49 +0530

    Done with inputs

--- B_Two_Buttons.java
+++ B_Two_Buttons.java
@@ -1,4 +1,9 @@
 import java.util.*;
 public class B_Two_Buttons {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+    }
 }
