f11b7898c781bcfe12c4d0b732d83921d30b2841
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-07 21:08:24 +0530

    Solved CF contest A. the equilizer

---
+++ A_The_Equalizer.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class A_The_Equalizer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            int k = sc.nextInt();
+            int sum = 0;
+            for(int i =0; i < n;i++) {
+                arr[i] = sc.nextInt();
+                sum += arr[i];
+            }
+            if(sum%2 != 0 || (k*n)%2 == 0) {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
