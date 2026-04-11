6d9a97eacdf10a568f1d19504c88d552f2572ea9
Nitesh7343 <niteshsingh7343@gmail.com>
2026-04-11 12:39:09 +0530

    Solved Conver in water tle 800 cf

---
+++ A_Cover_in_Water.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class A_Cover_in_Water {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            String s = sc.next();
+
+            if (s.contains("...")) {
+                System.out.println(2);
+            } 
+            else {
+                int count = 0;
+                for (char ch : s.toCharArray()) {
+                    if (ch == '.') count++;
+                }
+                System.out.println(count);
+            }
+        }
+        sc.close();
+    }
+}
