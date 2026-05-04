167f91a444596f9ff8ced0805938afe3e0dc2a48
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-04 21:56:52 +0530

    Fomatting code

--- AThreeIndices.java
+++ AThreeIndices.java
@@ -1,9 +1,13 @@
 import java.util.*;
 
-public class AThreeIndices {
-    public static void main(String[] args) {
+public class AThreeIndices 
+{
+    public static void main(String[] args) 
+    {
+
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+
         while(t-- > 0) {
 
             int n = sc.nextInt();
@@ -24,7 +28,8 @@ public class AThreeIndices {
 
             dq.clear();
 
-            for(int i = n-1; i >= 0;i--) {
+            for(int i = n-1; i >= 0;i--) 
+                {
                 while(!dq.isEmpty() && arr[dq.peek()] >= arr[i]) dq.pop();
 
                 nse[i] = dq.isEmpty() ? -1 : dq.peek();
@@ -32,20 +37,23 @@ public class AThreeIndices {
             }
 
             int[] ans = new int[3];
-            for(int i = 0; i < n;i++) {
-                if(nse[i] != -1 && pse[i] != -1) {
+            boolean found = false;
+
+            for(int i = 0; i < n;i++) 
+                {
+                if(nse[i] != -1 && pse[i] != -1) 
+                    {
                     ans[0] = pse[i];
                     ans[1] = i;
                     ans[2] = nse[i];
+                    System.out.println("YES");
+                    System.out.println(ans[0]+1+" "+ (ans[1]+1)+" "+(ans[2]+1));
+                    found = true;
                     break;
                 }
             }
 
-            if(ans[2] == 0) System.out.println("NO");
-            else {
-                System.out.println("YES");
-                System.out.println(ans[0]+1+" "+ (ans[1]+1)+" "+(ans[2]+1));
-            }
+            if(!found) System.out.println("NO");
         }
     }
 }
