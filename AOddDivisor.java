d995cbcb51d09944462d94abf45ace9a796e0d5a
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-05 23:37:49 +0530

    Formated after solving

--- AOddDivisor.java
+++ AOddDivisor.java
@@ -1,16 +1,21 @@
 import java.util.*;
 
-public class AOddDivisor {
-    public static void main(String[] args) {
+public class AOddDivisor 
+{
+    public static void main(String[] args) 
+{
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-- > 0) {
+        while(t-- > 0) 
+        {
             long n = sc.nextLong();
-            while(n % 2 ==  0) {
+            while(n % 2 ==  0) 
+            {
                 n /= 2;
             }
 
             System.out.println(n > 1 ? "YES" : "NO");
+            
         }
     }
 }
