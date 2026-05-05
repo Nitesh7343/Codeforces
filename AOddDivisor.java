eaed4ce5919cd4723a9cecc69f701adb451f5b2d
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-05 23:35:59 +0530

    Building intuattion for the solving the problem

--- AOddDivisor.java
+++ AOddDivisor.java
@@ -1,5 +1,12 @@
 import java.util.*;
 
 public class AOddDivisor {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            long n = sc.nextLong();
+            
+        }
+    }
 }
