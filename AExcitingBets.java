50e1a75d8dbfecd6a786cb0452f3052b2719d696
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-06 22:28:19 +0530

    input done for each test cases

--- AExcitingBets.java
+++ AExcitingBets.java
@@ -3,5 +3,9 @@ public class AExcitingBets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        while(t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+        }
     }
 }
