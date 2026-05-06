b46e9a48fe8c0b2fb57141364a3586e66eed267d
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-06 22:34:01 +0530

    formatting done que solved successfully

--- AExcitingBets.java
+++ AExcitingBets.java
@@ -1,21 +1,30 @@
 import java.util.*;
-public class AExcitingBets {
-    public static void main(String[] args) {
+public class AExcitingBets 
+{
+    public static void main(String[] args) 
+{
+
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-- > 0) {
+
+        while(t-- > 0) 
+        {
+
             long a = sc.nextLong();
             long b = sc.nextLong();
 
-            if(b > a) {
+            if(b > a)
+            {
                 long temp = a;
                 a = b;
                 b = temp;
             }
+
             long gcd = 0;
             long moves = 0;
             
-            if(a != b) {
+            if(a != b) 
+            {
                 gcd = a - b;
                 moves = Math.min(b%gcd,gcd-b%gcd);
             }
