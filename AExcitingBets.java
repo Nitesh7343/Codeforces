552e8e8fad9b4ca1e0e962dddc6a0a4a287b6874
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-06 22:27:37 +0530

    read no,. of test cases

--- AExcitingBets.java
+++ AExcitingBets.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class AExcitingBets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        int t = sc.nextInt();
     }
 }
