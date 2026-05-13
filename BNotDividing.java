d99034aea1f1bb737a01ec6a69165bcdbaf26a4f
Nitesh7343 <niteshsingh7343@gmail.com>
2026-05-13 23:06:16 +0530

    Solved and submitted

--- BNotDividing.java
+++ BNotDividing.java
@@ -14,14 +14,12 @@ public class BNotDividing {
                 nums[i] = sc.nextInt();
                 if(nums[i] == 1) nums[i]++;
             }
-            System.out.print(nums[0]+" ");
+
             for(int i = 0; i < n-1;i++) {
-                if(nums[i+1]%nums[i] == 0) 
-                {
-                    nums[i+1]++;
-                }
-                System.out.print(nums[i+1]+" ");
+                if(nums[i+1]%nums[i] == 0) nums[i+1]++;
             }
+
+            for(int num : nums) System.out.print(num+" ");
             System.out.println();
         }
     }
